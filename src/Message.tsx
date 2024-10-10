1b8a14db347618487fe01b1a93d6848ddaaef5fd
JaysonJAcosta5704 <JaysonJAcosta5704.coding@gmail.com>
2024-10-10 23:36:17 +0000

    Initial commit with half completed react app

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// Pascal Casing
+function Message() {
+	//JSK: Javascript XML
+	const name = '';
+	if (name)
+	 return <h1>Hello {name}</h1>;
+	return <h1>Hello World</h1>;
+}
+
+export default Message;
