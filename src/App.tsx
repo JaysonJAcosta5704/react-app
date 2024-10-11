a43320232cd100c58810c1285b02cf40a85cabc0
JaysonJAcosta5704 <JaysonJAcosta5704.coding@gmail.com>
2024-10-11 23:59:10 +0000

    End of react tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
 function App() {
- let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]
-
- return <div><ListGroup items={items} heading="Cities"/></div>;
+ return (
+ <div>
+  <Alert>
+   Hello <span>World!</span>
+  </Alert>
+ </div>
+ );
 }
 
 export default App;
